8b729c0a4513611f0d3a799c823a8aeaac7da7cb
Jordi Masramon <jordi.masramon@gmail.com>
2014-04-06 21:07:12 +0200

    Una mica mes sofisticat

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,26 @@ angular.module('angularAppApp')
       'AngularJS',
       'Karma'
     ];
-  });
+  })
+
+  .controller('TagManagerController', function() {
+    this.searchString = '';
+    this.SearchTags = [];
+    this.MatchingTags = [];
+
+    this.getTagsFromSearchString = function getTagsFromSearchString() {
+      this.SearchTags = this.searchString.split(' ');
+      return this.SearchTags;
+    };
+
+    this.searchTags = function searchTags() {
+      return ;
+    };
+
+    this.getMatchingTags = function getMatchingTags() {
+      this.MatchingTags = this.SearchTags.slice(1,2);
+      this.MatchingTags = this.MatchingTags.concat(this.SearchTags.slice(3,4));
+      return this.MatchingTags;
+    };
+  })
+;
